751a3caec2bd6f4a83858b375cdafc5b060f1cda
Sudhanshu Motewar <sudhanshumotewar2001@gmail.com>
2021-03-25 04:45:52 +0530

    Updated documentation

--- COVID-19 status/COVID-notifier.py
+++ COVID-19 status/COVID-notifier.py
@@ -1,19 +1,28 @@
-#!/usr/bin/env python
-# coding: utf-8
+"""
+    COVID-19 Status
+    -  Get notification of new cases and deaths
+
+    Author      : Sudhanshu Motewar
+    Start Date  : 21/03/21
+"""
 
 import requests
 from bs4 import BeautifulSoup
 from win10toast import ToastNotifier
 
+# get your country url here https://www.worldometers.info/coronavirus/
 url = 'https://www.worldometers.info/coronavirus/country/india/'
 response = requests.get(url)
 soup = BeautifulSoup(response.content, 'html.parser')
 
+# get the tag where death and cases is located
 news = soup.find("li",{"class":"news_li"})
 cases = news.find_all("strong")[0].text.split()[0]
 death = news.find_all("strong")[1].text.split()[0]
 
+# created object of ToastNotifier()
 notifier = ToastNotifier()
 message = "New Cases -----> " + cases + "\nDeath -----> " + death
 
+# it gives notification
 notifier.show_toast(title="COVID-19 STATUS", msg=message, duration=10, icon_path="coronavirus.ico")
