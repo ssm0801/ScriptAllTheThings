b5b75aaa4dbca614cc1becd5bdae18df38dc9fd2
Hershil Piplani <54028832+herkura@users.noreply.github.com>
2021-09-30 16:43:17 +0530

    Test file

---
+++ Convert To jpg/test.py
@@ -0,0 +1,17 @@
+# here’s a quick utility that will take the input file and convert it to a .jpg version using the same base name.
+import os
+import sys
+from PIL import Image
+
+if len(sys.argv) > 1:
+    if os.path.exists(sys.argv[1]):
+        im = Image.open(sys.argv[1])
+        target_name = sys.argv[1] + ".jpg"
+        rgb_im = im.convert('RGB')
+        rgb_im.save(target_name)
+        print("Saved as " + target_name)
+    else:
+        print(sys.argv[1] + " not found")
+else:
+    print("Usage: test.py <file>")
+ 
