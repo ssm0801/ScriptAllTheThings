84595f920d4c44ab31ed2b841106abd09fe9fb9a
Hershil Piplani <54028832+herkura@users.noreply.github.com>
2022-10-04 18:59:08 +0530

    Update googleSearch.py

--- Google Search automation/googleSearch.py
+++ Google Search automation/googleSearch.py
@@ -1,3 +1,12 @@
+'''
+Google Search automation
+
+- Solution to deal with repetitive google searching.
+
+Author : Hershil Piplani
+
+Date   : 4/10/2022
+'''
 from googlesearch import search
 results = search(term="Python Development", num_results=10, lang="en")
 results = list(results)
